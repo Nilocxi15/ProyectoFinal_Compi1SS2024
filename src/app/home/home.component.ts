2407929dc2e34481666fc28c1d763619921d02a6
Javier Desktop <javierixcolin202132163@cunoc.edu.gt>
2024-10-10 00:54:15 -0600

    Iniciando front de la página

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  line: number = 0;
+  column: number = 0;
 
+  title: string = '';
+
+  //Método para la posición del cursor
+  updateCursorPosition(event: Event): void {
+    const textarea = event.target as HTMLTextAreaElement;
+    const text = textarea.value.substring(0, textarea.selectionStart);
+    const lines = text.split('\n');
+    this.line = lines.length;
+    this.column = lines[lines.length - 1].length + 1;
+  }
+
+  //Método para leer archivo
+  triggerFileInput(): void {
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    fileInput.click();
+  }
+
+  //Método para un nuevo archivo
+  clearContent(): void {
+    const textarea = document.getElementById('scriptTextarea') as HTMLTextAreaElement;
+    textarea.value = '';
+    this.title = '';
+    this.line = 0;
+    this.column = 0;
+  }
 }
